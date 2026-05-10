29ce3d045e0c41758a9422e028819e235b7c8879
Abhishek Naik <agnaik21@gmail.com>
2026-05-10 04:39:38 -0700

    updated

--- src/steps/test.steps.js
+++ src/steps/test.steps.js
@@ -25,8 +25,10 @@ When('I enter valid credentials', async function () {
 
 Then('I should see the home page', async function (){});
 
+
+
 //Cart Dashboard page
-When('I click on the Cart button',{ timeout: 120000 }, async function () {
+When('I click on the Cart button', async function () {
 
   cartPage = new CartPage(this.page);
   await cartPage.clickCart();
